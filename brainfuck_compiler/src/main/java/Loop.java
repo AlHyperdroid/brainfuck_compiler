e0ccfe5571c539c4067b396fe0c2c6192f403f5c
AlHyperdroid <corzenoxnikita@gmail.com>
2021-08-30 10:14:13 +0300

    rename variables

--- brainfuck_compiler/src/main/java/Loop.java
+++ brainfuck_compiler/src/main/java/Loop.java
@@ -1,15 +1,15 @@
 import java.util.List;
 
 public class Loop implements ICommand {
-    private final List<ICommand> innerCommands;
+    private final List<ICommand> innerLoopCommands;
 
-    public Loop(List<ICommand> innerCommands) {
-        this.innerCommands = innerCommands;
+    public Loop(List<ICommand> innerLoopCommands) {
+        this.innerLoopCommands = innerLoopCommands;
     }
 
     public void execute(Memory memory) {
         while (memory.getCurrentCell() > 0) {
-            for (ICommand command : innerCommands) {
+            for (ICommand command : innerLoopCommands) {
                 command.execute(memory);
             }
         }
