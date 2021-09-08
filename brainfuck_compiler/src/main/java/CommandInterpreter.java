e35727bd032d817413ef2e3f6ed798613abf2cc0
AlHyperdroid <corzenoxnikita@gmail.com>
2021-09-08 18:04:42 +0300

    change CommandInterpreter logic

--- brainfuck_compiler/src/main/java/CommandInterpreter.java
+++ brainfuck_compiler/src/main/java/CommandInterpreter.java
@@ -1,40 +1,48 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class CommandInterpreter {
-    public List<ICommand> interpreter(String brainFuckCode) {
-        List<ICommand> innerLoopCommands = new ArrayList<>();
-        int index = 0;
-        int firstBracketIndex = 0;
-        char[] arrayBrainFuckCode = brainFuckCode.toCharArray();
+    private Map<Character, ICommand> innerCommandsMap;
 
-        for (int i = 0; i < brainFuckCode.length(); i++) {
-            if (arrayBrainFuckCode[i] == '[') {
-                if (index == 0)
-                    firstBracketIndex = i;
-                index++;
-            } else if (arrayBrainFuckCode[i] == ']') {
-                index--;
-                if (index == 0) {
-                    String loopInput = brainFuckCode.substring(firstBracketIndex + 1, i);
-                    innerLoopCommands.add(new Loop(interpreter(loopInput)));
-                }
-            } else if (index == 0) {
-                innerLoopCommands.add(getNecessaryOperation(arrayBrainFuckCode[i]));
-            }
-        }
-        return innerLoopCommands;
+    public CommandInterpreter() {
+        innerLoopCommandsMap();
     }
 
-    private ICommand getNecessaryOperation(Character character) {
-        return switch (character) {
-            case '+' -> new Increment();
-            case '-' -> new Decrement();
-            case '>' -> new NextCell();
-            case '<' -> new PreviousCell();
-            case '.' -> new OutputOperation();
-            default -> new InvalidOperation();
-        };
+    public List<ICommand> interpreter(String brainFuckCode) {
+        Deque<List<ICommand>> stack = new ArrayDeque<>();
+        stack.push(new ArrayList<>());
+
+
+        for (char character : brainFuckCode.toCharArray()) {
+
+            ICommand command = innerCommandsMap.get(character);
+
+            if (command == null) loopCommands(stack, character);
+
+            if (command != null) stack.peek().add(command);
+
+        }
+        return stack.pop();
+    }
+
+    private void loopCommands(Deque<List<ICommand>> stack, char character) {
+
+        if (character == '[') {
+            stack.push(new ArrayList<>());
+        }
+
+        if (character == ']') {
+            Loop loop = new Loop(stack.pop());
+            stack.peek().add(loop);
+        }
+    }
+
+    private void innerLoopCommandsMap() {
+        innerCommandsMap = new HashMap<>();
+        innerCommandsMap.put('+', new Increment());
+        innerCommandsMap.put('-', new Decrement());
+        innerCommandsMap.put('.', new OutputOperation());
+        innerCommandsMap.put('>', new NextCell());
+        innerCommandsMap.put('<', new PreviousCell());
     }
 }
 
